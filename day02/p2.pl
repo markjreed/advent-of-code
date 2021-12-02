b9f85ea94ca08ebb3dcc6be503ad6296899bc9ed
Mark J. Reed <markjreed@gmail.com>
2021-12-02 00:06:53 -0500

    day 2 solutions

---
+++ day02/p2.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+use v5.34;
+my ($aim, $pos, $depth) = (0,0,0);
+while (<>) {
+  if (/forward (\d+)/) { $pos+=$1; $depth += $aim *$1; }
+  elsif (/down (\d+)/) { $aim+=$1 }
+  elsif (/up (\d+)/) { $aim-=$1 }
+}
+say $pos * $depth;
+
