601f5022177bdf859e74c96d7a7ce71cf407f9c7
Mark J. Reed <markjreed@gmail.com>
2022-12-01 18:27:11 -0500

    feat: consolidate parts into single solution file per language

--- 2022/day01/part1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import sys
-with (open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin) as source:
-    elves = [sum([int(n) for n in elf.split("\n") if len(n)])
-                         for elf in source.read().split("\n\n")]
-print(max(elves))
