9bb32bbc5811cb691875092d2fc2ab2be3d1f92e
Mark J. Reed <markjreed@gmail.com>
2022-12-01 15:24:32 -0500

    feat: add Python versions

---
+++ 2022/day01/part2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import sys
+with (open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin) as source:
+    elves = [sum([int(n) for n in elf.split("\n") if len(n)])
+                         for elf in source.read().split("\n\n")]
+print(sum(sorted(elves,reverse=True)[0:3]))
