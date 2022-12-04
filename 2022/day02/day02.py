96ec179f034c60cd020495fb8f63bae1bfd78eb6
Mark J. Reed <markjreed@gmail.com>
2022-12-04 16:12:40 -0500

    feat: format python

--- 2022/day02/day02.py
+++ 2022/day02/day02.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import sys
+
 with (open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin) as source:
-    matches = [3*'ABC'.index(e)+'XYZ'.index(h) for e,h in [line[:-1].split(' ')
-                                               for line in source]]
-# See notes.txt for derivation of score formulas
-Part1 = [3 * ((h - e + 1)%3) + h + 1 for e in range(3) for h in range(3)]
+    matches = [3*'ABC'.index(e)+'XYZ'.index(h) for e, h in [line[:-1].split(' ')
+                                                            for line in source]]
+
+# Precomputed scores of all possible matchups. See notes.txt for derivation of
+# formulas.
+Part1 = [3 * ((h - e + 1) % 3) + h + 1 for e in range(3) for h in range(3)]
 Part2 = [3 * h + (e + h + 2) % 3 + 1 for e in range(3) for h in range(3)]
+
 print(f'Part 1: {sum([Part1[m] for m in matches])}')
 print(f'Part 2: {sum([Part2[m] for m in matches])}')
