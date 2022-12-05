f15c7c6ba32902b73be9607b65f4a08105da02c7
Mark J. Reed <markjreed@gmail.com>
2022-12-05 09:52:08 -0500

    feat: parse as much as possible on first pass

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+
 picture = []
 moves = []
 labels = None
@@ -11,24 +12,26 @@ with (open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin) as source:
             labels = cols
         elif labels:
             if len(row):
-                moves.append(row)
+                moves.append([cols[i] for i in range(1,6,2)])
         else:
-            picture.append([row[i] for i in range(1,len(line),4)])
+            picture.append([row[i] for i in range(1, len(line), 4)])
 
+# parse starting configuration
 start = [[crate for crate in stack if crate != ' '] for stack in zip(*picture)]
 
-for part in range(1,3):
-    stacks = { label: stack.copy() for label, stack in zip(labels, start) }
+for part in range(1, 3):
 
-    for move in moves:
-        words = move.split()
-        count, source, dest = [words[i] for i in range(1,6,2)]
+    # reset to starting config at top of sim
+    stacks = {label: stack.copy() for label, stack in zip(labels, start)}
+
+    # then execute the moves
+    for count, source, dest in moves:
         n = int(count)
         if part == 1:
             for _ in range(n):
-                stacks[dest].insert(0,stacks[source].pop(0))
+                stacks[dest].insert(0, stacks[source].pop(0))
         else:
             stacks[dest] = stacks[source][:n] + stacks[dest]
-            stacks[source]  = stacks[source][n:]
+            stacks[source] = stacks[source][n:]
 
     print(f'Part {part}: {"".join([stacks[k][0] for k in sorted(stacks)])}')
