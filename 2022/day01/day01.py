686ab4bc9952ee66a9ebe802c381bca7a5719cab
Mark J. Reed <markjreed@gmail.com>
2022-12-05 09:55:01 -0500

    fix: consistent output formatting

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -5,5 +5,5 @@ with (open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin) as source:
     elves = [sum([int(n) for n in elf.split("\n") if len(n)])
             for elf in source.read().split("\n\n")]
 
-print(f'Part 1:{max(elves)}')
-print(f'Part 2:{sum(sorted(elves,reverse=True)[0:3])}')
+print(f'Part 1: {max(elves)}')
+print(f'Part 2: {sum(sorted(elves,reverse=True)[0:3])}')
