386b93f8c8e3516fb0b00a691b1846d33d547984
Mark J. Reed <markjreed@gmail.com>
2022-12-05 23:05:10 -0500

    commentary

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -4,22 +4,32 @@ import sys
 picture = []
 moves = []
 labels = None
+
+# read our input
 with (open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin) as source:
+    # a line at a time
     for line in source:
         row = line.rstrip('\n')
         cols = row.split()
+
         if len(cols) and cols[0].isnumeric():
+            # remember the stack labels
             labels = cols
         elif labels:
+            # if we've seen the labels and the line isn't blank
             if len(row):
+                # then this is a move instruction; parse out its parameters
                 moves.append([cols[i] for i in range(1,6,2)])
         else:
+            # if we haven't seen the labels yet, this is part of the picture
+            # represetnting the starting configuration
             picture.append([row[i] for i in range(1, len(line), 4)])
 
-# parse starting configuration
+# transpose starting configuration so each stack is a list
 start = [[crate for crate in stack if crate != ' '] for stack in zip(*picture)]
 
-for part in range(1, 3):
+# run the simulation twice, once for each part
+for part in range(2):
 
     # reset to starting config at top of sim
     stacks = {label: stack.copy() for label, stack in zip(labels, start)}
@@ -27,10 +37,12 @@ for part in range(1, 3):
     # then execute the moves
     for count, source, dest in moves:
         n = int(count)
-        if part == 1:
+        if not part:
+            # for part 1, move one crate at a time
             for _ in range(n):
                 stacks[dest].insert(0, stacks[source].pop(0))
         else:
+            # for part 2, move all N crates at once
             stacks[dest] = stacks[source][:n] + stacks[dest]
             stacks[source] = stacks[source][n:]
 
