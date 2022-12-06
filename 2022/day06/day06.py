5d49987d833c189dd7a82b24a11f525f2c3c6959
Mark J. Reed <markjreed@gmail.com>
2022-12-06 09:56:02 -0500

    feat: python solution for day 6

---
+++ 2022/day06/day06.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+
+# read our input
+with (open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin) as source:
+    # a line at a time
+    for line in source:
+        data = line.rstrip('\n')
+        for part in range(2):
+            size = 4 + part * 10
+            for i in range(size,len(data)):
+                if len(set(data[i-size:i])) == size:
+                    print(f'Part {part+1}: {i}')
+                    break
