14eeed9a48873b97b65c0454cc68298feda56d25
Mark J. Reed <markjreed@gmail.com>
2023-12-22 15:13:09 -0500

    feat: solution for day 5 part 2

--- 2023/day05/part2.py
+++ 2023/day05/part2.py
@@ -4,6 +4,7 @@ from intervaltree import Interval, IntervalTree
 
 def main(input_file):
     tree = IntervalTree()
+    new = None
     with open(input_file) as f:
         for line in f:
             words = line.split()
@@ -15,30 +16,35 @@ def main(input_file):
                     start = int(words.pop(0))
                     length = int(words.pop(0))
                     tree.addi(start, start+length)
-                print(tree)
+                #print(tree)
             elif '-to-' in words[0]:
+                if new != None: 
+                    tree |= new
+                    #print(tree)
+                new = IntervalTree()
                 continue
             else:
                 to, start, length = [int(w) for w in words]
+                #print(f'mapping {start}...{start+length-1} to {to}...{to+length-1}')
                 stop = start + length
                 mapping = Interval(start, start+length)
                 for i in tree[mapping.begin:mapping.end]:
                     begin = i.begin
                     end = i.end
                     if begin < start:
-                        before = Interval(begin, start-1)
+                        before = Interval(begin, start)
                         tree.add(before)
                         begin = start
                     if end > stop:
-                        after = Interval(stop+1, end)
+                        after = Interval(stop, end)
                         tree.add(after)
                         end = stop
                     begin = begin - start + to
                     end   = end - start + to
                     tree.discard(i)
-                    tree.addi(begin, end)
-                print(tree)
+                    new.addi(begin, end)
 
+    tree |= new
     print(tree.begin())
 
 if __name__ == '__main__':
