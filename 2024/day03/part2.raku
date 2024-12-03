48157b21724d845a024f9688fa197b96d986e39d
Mark J. Reed <markjreed@gmail.com>
2024-12-03 11:29:11 -0500

    fix: consistent names between implementations

--- 2024/day03/part2.raku
+++ 2024/day03/part2.raku
@@ -1,9 +1,9 @@
 #!/usr/bin/env raku
 my $doing = True;
-my $sum = 0;
+my $total = 0;
 for slurp() ~~  m:g/('mul') '(' ( \d ** 1..3 ) ',' ( \d ** 1..3 ) ')' || ( 'do' "n't"? ) '()' / -> $/ {
     when ~$0 eq "do"    { $doing = True }
     when ~$0 eq "don't" { $doing = False }
-    when ~$0 eq "mul"   { $sum += $1 * $2 if $doing }
+    when ~$0 eq "mul"   { $total += $1 * $2 if $doing }
 }
-say $sum;
+say $total;
--- 2024/day03/part2.sh
+++ 2024/day03/part2.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-skip=0
 total=0
+doing=1
 while read line; do
     case "$line" in
-        ('do()') skip=0;;
-        ($'don\'t()') skip=1;;
+        ('do()') doing=1;;
+        ($'don\'t()') doing=0;;
         ('mul'*) 
-            if (( ! skip )); then
+            if (( doing )); then
                 read x y <<<"${line//[^0-9]/ }"
                 (( total += x * y ))
             fi;;
     esac
-done < <(grep -o $'mul([0-9]\{1,3\},[0-9]\{1,3\})\|do()\|don\'t()' data.txt)
+done < <(grep -o $'mul([0-9]\{1,3\},[0-9]\{1,3\})\|do()\|don\'t()')
 echo $total
