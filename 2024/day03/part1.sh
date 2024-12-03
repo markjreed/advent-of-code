0a1464503e9543b63b4325bb0f218002b863b874
Mark J. Reed <markjreed@gmail.com>
2024-12-03 07:34:37 -0500

    fix: formatting

--- 2024/day03/part1.sh
+++ 2024/day03/part1.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-grep -o 'mul([0-9]\{1,3\},[0-9]\{1,3\})' data.txt | gsed -e 's/[^0-9]/ /g' -e 's/$/* + /' -e '1s/+//' -e '$s/$/ p/' | dc 
+grep -o 'mul([0-9]\{1,3\},[0-9]\{1,3\})' data.txt |
+sed -e 's/[^0-9]/ /g' -e 's/$/* + /' -e '1s/+//' -e '$s/$/ p/' | dc
