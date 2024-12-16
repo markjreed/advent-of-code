d061598b13b976c491c0c66b81270f46565ecf75
Mark J. Reed <markjreed@gmail.com>
2024-12-16 13:48:52 -0500

    feat: 2024 day 16 part 2 complete

--- day16/day16.py
+++ day16/day16.py
@@ -52,23 +52,30 @@ def main(filename, start_dir=1):
                 graph.add_edge(self, neighbor, StepCost)
 
     start = nodes[start_at + ( start_dir, )]
-    paths = rx.dijkstra_shortest_paths(graph, start, weight_fn = float)
     
-    shortest = math.inf
+    best = (math.inf,[])
     for d in range(4):
         key = end_at + (d,)
         if key in nodes:
             finish = nodes[key]
+            paths = rx.all_shortest_paths(graph, start, finish,
+                                              weight_fn = float)
             last = None
             cost = 0
-            for node in paths[finish]:
+            for node in paths[0]:
                 if last is not None:
                     cost += graph.get_edge_data(last, node)
                 last = node
-            if cost < shortest:
-                shortest = cost
-
-    print(shortest)
+            if cost < best[0]:
+                best = (cost, paths)
+    print(best[0])
+    good_seats = {}
+    for path in best[1]:
+        for node in path:
+            key = graph.get_node_data(node)
+            i,j,d = key
+            good_seats[(i,j)] = True
+    print(len(good_seats))
 
 if __name__ == '__main__' and len(sys.argv) > 1:
     main(sys.argv[1])
