5ac1d3b37417e3d630e247213b36abdb311b3d9d
Mark J. Reed <markjreed@gmail.com>
2025-01-10 09:43:44 -0500

    feat: 2015 day 3 part 2

--- 2015/day03/day3.sh
+++ 2015/day03/day3.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env bash
 main() {
-    local input_file=${1--} x=0 y=0 dir
-    local -A gifts=(["0,0"]=1)
+    local input_file=${1--} 
+    local x1=(0)   y1=(0)   # Just one deliverer in part 1
+    local x2=(0 0) y2=(0 0) # Two deliverers in part 2
+    local -A part1 part2 i j
+    for i in "${!x1[@]}"; do
+        local x=${x1[i]} y=${y1[i]}
+        (( part1[$x,$y]++ ))
+    done
+    for j in "${!x2[@]}"; do
+        local x=${x2[j]} y=${y2[j]}
+        (( part2[$x,$y]++ ))
+    done
+
+    local turn=0
     while read dir; do
+        (( i = turn % ${#x1[@]} ))
+        (( j = turn % ${#x2[@]} ))
         case "$dir" in
-            '^') (( y+=1 ));;
-            '>') (( x+=1 ));;
-            'v') (( y-=1 ));;
-            '<') (( x-=1 ));;
+            '^') (( y1[i]+=1, y2[j]+=1 ));;
+            '>') (( x1[i]+=1, x2[j]+=1 ));;
+            'v') (( y1[i]-=1, y2[j]-=1 ));;
+            '<') (( x1[i]-=1, x2[j]-=1 ));;
         esac
-        (( gifts["$x,$y"]++ ))
+        (( part1["${x1[i]},${y1[i]}"]++ ))
+        (( part2["${x2[j]},${y2[j]}"]++ ))
+        (( turn += 1 ))
     done < <(grep -o . "$input_file")
-    printf '%d\n' "${#gifts[@]}"
+    printf '%d\n' "${#part1[@]}"
+    printf '%d\n' "${#part2[@]}"
 }
 
 main "$@"
