69f39f00c6d2737d1ff5ef9d16900098baefdd32
Mark J. Reed <markjreed@gmail.com>
2025-01-10 10:03:29 -0500

    feat: 2015 day 4 part 2 (Ruby)

--- 2015/day04/day4.rb
+++ 2015/day04/day4.rb
@@ -4,14 +4,20 @@ input_file = ARGV[0]
 md5 = Digest::MD5.new
 File.open(input_file, "r") do |fh|
     fh.each_line do |prefix|
+        part1 = nil
+        part2 = nil
         md5.reset
         prefix.chomp!
         number = 1
-        while not md5.hexdigest.start_with?('00000') 
+        while not md5.hexdigest.start_with?('000000') 
+            if md5.hexdigest.start_with?('00000') and part1.nil? then
+                part1 = number - 1
+            end
             md5.reset
             md5 << "#{prefix}#{number}"
             number += 1
         end
+        puts part1
         puts number - 1
     end
 end
