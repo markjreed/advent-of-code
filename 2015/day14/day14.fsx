b7ed95af40d6a0b69fdd5200814c1e977d22c35d
Mark J. Reed <markjreed@gmail.com>
2025-01-23 17:40:57 -0500

    wip: 2015 day 14 (F#), need to fix tie handling

--- 2015/day14/day14.fsx
+++ 2015/day14/day14.fsx
@@ -27,27 +27,54 @@ for line in readLines(args[2]) do
     let restTime = words[13] |> int
     reindeer.Add(new Reindeer(name, speed, flyTime, restTime));;
 
-let position = ResizeArray<int>(Array.create reindeer.Count 0);;
 
+let winner duration : int * int = 
+    let mutable windex = -1
+    let mutable maxDist = -1
+    let mutable position = ResizeArray<int>(Array.create reindeer.Count 0)
+    //printf "%d" <| duration
+
+    for i in 0 .. reindeer.Count - 1  do
+        let deer = reindeer[i]
+        let period = deer.FlyTime + deer.RestTime
+        let distance = deer.Speed * deer.FlyTime
+        let complete = duration / period
+        let partial  = duration % period
+        let extra = if partial > deer.FlyTime then deer.FlyTime else partial
+        position[i] <- complete * distance + extra * deer.Speed
+        //printf ", %d" <| position[i]
+        if position[i] = maxDist then
+            windex <- -1;
+        if position[i] > maxDist then 
+            maxDist <- position[i]
+            windex <- i;
+    (windex, maxDist);;
+    
 let duration = args[3] |> int;;
-let mutable winner = -1;;
-let mutable maxDist = 0;;
+
+// part 1
+let (deerNum, dist) = winner(duration);;
+printfn "%d" <| dist;;
+
+// part 2
+let mutable score = ResizeArray<int>(Array.create reindeer.Count 0)
+for time in 1 .. duration do
+    let (won, dist) = winner(time)
+    if won >= 0 then
+        score[won] <- score[won] + 1
+    else
+        for i in 0 .. reindeer.Count - 1  do
+            score[i] <- score[i] + 1
+//    for i in 0 .. reindeer.Count - 1 do 
+//        printf ", %d" score[i]
+//    printfn ""
+
+let mutable windex = -1
+let mutable maxScore = 0
 
 for i in 0 .. reindeer.Count - 1  do
-    let deer = reindeer[i]
-    let period = deer.FlyTime + deer.RestTime;
-    let distance = deer.Speed * deer.FlyTime
-    let complete = duration / period
-    let partial  = duration % period
-    let extra = if partial > deer.FlyTime then deer.FlyTime else partial
-    position[i] <- complete * distance + extra * deer.Speed
-    if position[i] > maxDist then 
-        maxDist <- position[i]
-        winner <- i
-    
-//    printfn "%s (%d km/s %d/%d seconds) is at %d" <| deer.Name <| deer.Speed <| deer.FlyTime <| deer.RestTime <| position[i]
-    ;;
+    if score[i] > maxScore then
+        maxScore <- score[i]
+        windex <- i
 
-printfn "The winner is %s at distance %d km" <| reindeer[winner].Name  <| position[winner];;
-
-
+printfn "%d" <| score[windex]
