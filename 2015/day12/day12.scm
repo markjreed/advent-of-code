b95727442b9b25487dc4fdf4c21cb1693ae7b113
Mark J. Reed <markjreed@gmail.com>
2025-01-23 09:01:14 -0500

    feat: 2015 day 12 complete (Scheme)

--- 2015/day12/day12.scm
+++ 2015/day12/day12.scm
@@ -11,13 +11,24 @@
 (define data 
   (with-input-from-file (car (command-line-arguments)) read-json))
 
-(define (add-numbers obj subtotal) 
-    (cond ((boolean? obj) subtotal)
-          ((pair? obj) (add-numbers (cdr obj) (add-numbers (car obj) subtotal)))
+(define (swap obj) (cons (cdr obj) (car obj)))
+
+(define (red-object? obj)
+    (and (pair? obj) (pair? (car obj)) (assoc "red" (map swap obj))))
+
+; read-json maps objects to association lists and arrays to vectors.
+; recursively add up the numbers contained in the data, optionally skipping
+; all objects that contain a property with the value "red"
+(define (add-numbers obj . rest) (let-optionals rest ((subtotal 0) (skip-red #f))
+    (cond ((and (vector? obj) (not (zero? (vector-length obj))))
+               (let ((first (add-numbers (vector-ref obj 0) subtotal skip-red)))
+                    (add-numbers (subvector obj 1) first skip-red)))
+          ((and (pair? obj) (or (not skip-red) (not (red-object? obj))))
+               (let* ((values (map cdr obj))
+                      (first (add-numbers (car values) subtotal skip-red)))
+                     (add-numbers (list->vector (cdr values)) first skip-red)))
           ((number? obj) (+ subtotal obj))
-          ((vector? obj) 
-            (cond ((zero? (vector-length obj)) subtotal)
-                  (#t (add-numbers (subvector obj 1) (add-numbers (vector-ref obj 0) subtotal)))))
-          (#t subtotal)))
-                  
-(format #t "~a~%" (add-numbers data 0))
+          (#t subtotal))))
+
+(format #t "~a~%" (add-numbers data))
+(format #t "~a~%" (add-numbers data 0 #t))
